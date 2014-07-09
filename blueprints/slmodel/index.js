2bd3b89a2e68370186fe50b9444a8ccc6d89ef23
Matthew Marcum <mmarcum@softlayer.com>
2014-07-09 17:32:35 -0500

    fixed bug in slmodel blueprint

--- blueprints/slmodel/index.js
+++ blueprints/slmodel/index.js
@@ -1,9 +1,9 @@
 
 module.exports = {
     locals: function( options ){
-        var locals = {},
-            url = options.entity.options.url,
-            adpater: options.entity.options.adapter;
+        var locals  = {},
+            url     = options.entity.options.url,
+            adpater = options.entity.options.adapter;
 
         return {
             url: url ? url : "'';throw 'Please add a URL to model: "+entity.name+"';",
