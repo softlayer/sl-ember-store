257fe91a479bba91db58f49d63623dbf02be74f2
Jeremy Brown <jebrown@softlayer.com>
2014-10-10 09:45:52 -0500

    Refactor to follow style guides

--- tests/unit/store-test.js
+++ tests/unit/store-test.js
@@ -13,19 +13,19 @@ var Foo,
 
 module( 'Unit - sl-model/store', {
 
-    setup: function(){
+    setup: function() {
         Foo = Model.extend();
         Bar = Model.extend();
         Bar.reopenClass({ adapter: 'localstorage' });
-        
-        AjaxAdapter = Ember.Object.extend({ 
-            type: 'ajax', 
-            __find: function(){}, 
-            find: function(){ 
-                return Ember.RSVP.resolve( [ Ember.Object.create() ] ); 
-            }  
+
+        AjaxAdapter = Ember.Object.extend({
+            type: 'ajax',
+            __find: function(){},
+            find: function(){
+                return Ember.RSVP.resolve( [ Ember.Object.create() ] );
+            }
         });
-        
+
         LocalstorageAdapter = Ember.Object.extend({ type: 'localstorage' });
 
         store = Store.create({
@@ -52,7 +52,7 @@ module( 'Unit - sl-model/store', {
 
         store.container.registry.push( { key: 'model:foo', factory: Foo } );
         store.container.registry.push( { key: 'model:bar', factory: Bar } );
-        
+
         ajaxAdapter = store.container.lookup('adapter:ajax');
 
 
@@ -64,7 +64,7 @@ module( 'Unit - sl-model/store', {
         sinon.spy( Foo, 'create' );
         queryHook = sinon.spy();
     },
-    teardown: function(){
+    teardown: function() {
         store.__find.restore();
         store.modelFor.restore();
         store.adapterFor.restore();
@@ -83,14 +83,14 @@ test( 'modelFor: should return the model "Bar" for type "bar" ', function() {
 });
 
 test( 'adapterFor: should return the adapter ajax for model type foo', function() {
-    ok( store.adapterFor( 'foo' ) instanceof AjaxAdapter ); 
+    ok( store.adapterFor( 'foo' ) instanceof AjaxAdapter );
 });
 
 test( 'adapterFor: should return the adapter localstorage for model type bar', function() {
-    ok( store.adapterFor( 'bar' ) instanceof LocalstorageAdapter ); 
+    ok( store.adapterFor( 'bar' ) instanceof LocalstorageAdapter );
 });
 
-test( 'findOne: should call __find with correct args', function(){
+test( 'findOne: should call __find with correct args', function() {
     var options = { "otherId":1 },
         args;
 
@@ -99,59 +99,59 @@ test( 'findOne: should call __find with correct args', function(){
     ok( store.__find.calledWith( 'foo', null, options, true ) );
 });
 
-test( 'find should call __find with numeric id', function(){
+test( 'find should call __find with numeric id', function() {
     var options = { "otherId": 1 };
     store.find( 'foo', 1, options );
     ok( store.__find.calledWith( 'foo', 1, options, false ) );
 });
 
-test( 'find should call __find with object for first param', function(){
+test( 'find should call __find with object for first param', function() {
     var options = { "otherId": 1 };
     store.find( 'foo', options );
     ok( store.__find.calledWith( 'foo', null, options, false ) );
 });
 
-test( 'find should call __find with only the type', function(){
+test( 'find should call __find with only the type', function() {
     store.find( 'foo' );
     ok( store.__find.calledWith( 'foo', null, null, false ) );
 });
 
-test( '__find should have called modelFor', function(){
+test( '__find should have called modelFor', function() {
     store.__find( 'foo', 1, {}, false );
     ok( store.modelFor.calledWith( 'foo' ) );
 });
 
-test( '__find should have called adapterFor', function(){
+test( '__find should have called adapterFor', function() {
     store.__find( 'foo', 1, {}, false );
     ok( store.adapterFor.calledWith( 'foo' ) );
 });
 
-test( '__find should have called AjaxAdapter.find', function(){
+test( '__find should have called AjaxAdapter.find', function() {
     store.__find( 'foo', 1, {}, false );
     ok( ajaxAdapter.find.calledWith( 'foo', 1, {}, false ) );
 });
 
-test( 'createRecord should have called modelFor', function(){
+test( 'createRecord should have called modelFor', function() {
     store.createRecord( 'foo' );
     ok( store.modelFor.calledWith( 'foo' ) );
 });
 
-test( 'createRecord should have called Foo.create once', function(){
+test( 'createRecord should have called Foo.create once', function() {
     store.createRecord( 'foo' );
     ok( Foo.create.calledOnce );
 });
 
-test( 'createRecord should have called Foo.create with an object container', function(){
+test( 'createRecord should have called Foo.create with an object container', function() {
     store.createRecord( 'foo' );
     ok( Foo.create.calledWith( { container: store.container } ) );
 });
 
-test( 'registerPreQueryHook should add an entry to preQueryHooks', function(){
+test( 'registerPreQueryHook should add an entry to preQueryHooks', function() {
     store.registerPreQueryHook( queryHook );
     ok( store.get( 'preQueryHooks' ).length === 1 );
 });
 
-test( 'runPreQueryHooks should run query hook once', function(){
+test( 'runPreQueryHooks should run query hook once', function() {
     store.registerPostQueryHook( queryHook );
     store.runPostQueryHooks();
     ok( queryHook.calledOnce );
