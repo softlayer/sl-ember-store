26ab5d2e67611a7a97376b8839aa0b36e8777525
Matthew Marcum <mmarcum@softlayer.com>
2014-10-28 16:32:56 -0500

    wip adding demo app and acceptance test

---
+++ tests/dummy/app/initializers/pretender.js
@@ -0,0 +1,58 @@
+import Pretender from 'pretender';
+
+var fooRecords = [
+    {
+        id: 1,
+        text: 'This is foo record with id: 1',
+        bar: {
+            id: 1,
+            text: 'This is a bar record with id: 1'
+        }
+    },
+    {
+        id: 2,
+        text: 'This is foo record #2',
+        bar: {
+            id: 2,
+            text: 'This is a bar record with id: 2'
+        }
+
+    },
+    {
+        id: 3,
+        text: 'This is foo record #3',
+        bar: {
+            id: 3,
+            text: 'This is a bar record with id: 3'
+        }
+
+    }
+];
+
+export function initialize(/* container, application */) {
+    new Pretender(function(){
+        this.get( '/foo', function(request){
+            var id = request.queryParams.id && ( parseInt( request.queryParams.id ) - 1 );
+
+            if( request.queryParams.id ){
+                return [ 
+                    200, 
+                    { "Content-Type":"application/json" }, 
+                    JSON.stringify( fooRecords[ id ] )
+                ];
+            }
+            
+            return [
+                200,
+                { "Content-Type":"application/json" }, 
+                JSON.stringify( fooRecords )
+            ];
+            });
+    });
+}
+
+export default {
+  name: 'pretender',
+  after: 'sl-model',
+  initialize: initialize
+};
--- tests/dummy/app/routes/single-model.js
+++ tests/dummy/app/routes/single-model.js
@@ -1,6 +1,6 @@
 import Ember from 'ember';
 export default Ember.Route.extend({
     model: function( params ){
-        return Ember.Object.create({id:params.model_id});
+        return this.store.find( 'foo', params.model_id );
     }
 });
